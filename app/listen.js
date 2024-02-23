6b397374021dfa634ce4d16a341e19d5a6130db1
Basil kelly <basiljkelly@outlook.com>
2024-02-23 11:30:33 +0000

    hosting functionality added

--- app/listen.js
+++ app/listen.js
@@ -1,8 +1,4 @@
-app.listen(9090, (err)=>{
-    if(err) {
-        console.log(err)
-    }
-    else{
-        console.log('Listening on port 9090')
-    }
-}) 
+const app = require('./app.js');
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
