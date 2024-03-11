fcb6f7e69b1f621bea71c250090e5e74335ddce4
Basil kelly <basiljkelly@outlook.com>
2024-03-11 10:30:00 +0000

    allow cross origin resource sharing

--- app/app.js
+++ app/app.js
@@ -10,7 +10,10 @@ const {
   deleteComment,
   getUsers,
 } = require("../controllers/controller");
+
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 
